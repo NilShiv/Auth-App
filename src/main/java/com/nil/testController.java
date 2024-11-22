b934303974b95e280ba45ad48c8dd1e2a01caf9c
ADMIN <ADMIN@DESKTOP-689T5NO.bbrouter>
2024-11-22 17:03:13 +0530

    second commit

--- src/main/java/com/nil/testController.java
+++ src/main/java/com/nil/testController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
 	
 	@GetMapping("/")
-	public String test(){
+	public String tests(){
 		return "jai mahakal";
 	}
 }
